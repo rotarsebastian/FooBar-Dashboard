3e7608032c91e6e278ff120553c8379fc077ad06
Sebastian Rotar <sebastianrotar@Sebastians-MacBook-Pro.local>
2018-10-09 14:00:35 +0200

    pictures added

--- index.js
+++ index.js
@@ -3,4 +3,24 @@ import GreenSock from gsap;
 import Chart from Chart.js;
 */
 
+init();
+
+const data = FooBar.getData();
+const newData = JSON.parse(data);
+
+function init() {
+    //setInterval(update, 300);
+    update();
+}
+
+function update() {
+    let data = JSON.parse(FooBar.getData());
+    console.log(FooBar.getData(true));
+    console.log(data.bartenders);
+    handleBartender(data.bartenders);
+}
+
+function handleBartender(bartender) {
+    console.log(bartender[0]);
+}
 
